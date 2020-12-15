d6ae2a3565762a26e2e9de952c96fd95132de4cb
hayubnustseecs <hayub.bese18seecs@seecs.edu.pk>
2020-12-16 01:35:10 +0500

    initial commit

---
+++ JS/String Part 2.js
@@ -0,0 +1,7 @@
+var str1 = "Today is"
+var str2 = "A beautiful day"
+var str3 = "In Hawaii"
+
+var res = str1 + " " + str2.charAt(0).toLowerCase() + str2.slice(1) + " " + str3.charAt(0).toLowerCase() + str3.slice(1)
+
+console.log(res)
