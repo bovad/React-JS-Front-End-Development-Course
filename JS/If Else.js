1038acb29a950c4ad0f74a1112e1d419b897b0f6
hayubnustseecs <hayub.bese18seecs@seecs.edu.pk>
2020-12-16 02:01:42 +0500

    Initial commit

---
+++ JS/If Else.js
@@ -0,0 +1,10 @@
+// Practice Problems
+
+// Question 01
+var length = 200;
+var width = 200;
+if(length * width == length * length){
+    console.log("Square")
+}else{
+    console.log("Rectangle")
+}
