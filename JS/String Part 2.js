08aa1fba56327fe24139f59742fa3fc66ba70e1a
hayubnustseecs <hayub.bese18seecs@seecs.edu.pk>
2020-12-16 01:42:19 +0500

    initial commit

--- JS/String Part 2.js
+++ JS/String Part 2.js
@@ -1,3 +1,5 @@
+// Question 01
+
 var str1 = "Today is"
 var str2 = "A beautiful day"
 var str3 = "In Hawaii"
@@ -5,3 +7,10 @@ var str3 = "In Hawaii"
 var res = str1 + " " + str2.charAt(0).toLowerCase() + str2.slice(1) + " " + str3.charAt(0).toLowerCase() + str3.slice(1)
 
 console.log(res)
+
+// Question 02
+
+var mStr = 'Mo Tu We Th Fr Sa Su'
+mStr = mStr.toUpperCase()
+var days = mStr.split(" ")
+console.log(days)
