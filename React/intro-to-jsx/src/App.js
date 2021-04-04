88a1ddeb37a06021b5907aab653a42bf30d36b6d
yassergithub1 <yassergithub1@gmail.com>
2021-04-04 14:08:20 +0500

    Creating React Elements Dynamically

--- React/intro-to-jsx/src/App.js
+++ React/intro-to-jsx/src/App.js
@@ -1,13 +1,41 @@
 import './App.css';
 
 function App() {
+  const myArray = [
+    {
+      id: 1,
+      title: 'Title 1',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    },
+    {
+      id: 2,
+      title: 'Title 2',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    },
+    {
+      id: 3,
+      title: 'Title 3',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    },
+    {
+      id: 4,
+      title: 'Title 4',
+      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+    }
+  ]
 
+  const arrIter = myArray.map(item => {
+      return (
+        <div className="article">
+          <h1 className='heading'>{item.title}</h1>
+          <p>{item.description}</p>
+        </div>
+      )
+  })
   return (
     <div className="App">
-      <div>
-        <h1 className="heading">Styling React Elements</h1>
-        <p className="para">This is an introduction to JSX Styling React Elements</p>
-      </div>
+      <h1 className='heading'>Creating React Elements Dynamically</h1>
+      {arrIter}
     </div>
   );
 }
