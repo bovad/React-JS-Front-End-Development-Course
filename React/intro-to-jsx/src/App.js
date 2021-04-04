b0398a6e8f05372b52f5e48972f33fa68e695003
yassergithub1 <yassergithub1@gmail.com>
2021-04-04 13:33:24 +0500

    Styling React Elements Part-2

--- React/intro-to-jsx/src/App.js
+++ React/intro-to-jsx/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,20 +5,8 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1 style = {
-        {
-          color: 'Red',
-        }
-        }>Styling React Elements</h1>
-        <p style = {
-          {
-            border: '1px solid red',
-            width: '500px',
-            margin: 'auto',
-            padding: '50px',
-            boxShadow: '5px 5px 5px red inset'
-          }
-        }>This is an introduction to JSX Styling React Elements</p>
+        <h1 className="heading">Styling React Elements</h1>
+        <p className="para">This is an introduction to JSX Styling React Elements</p>
       </div>
     </div>
   );
